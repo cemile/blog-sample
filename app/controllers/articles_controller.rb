a86c2297c8c53994fbd03ddc84fb22046ca4784b
Cemile Sarıcaoğlu <saricaoglucemile@gmail.com>
2014-08-17 17:43:08 +0300

    if kontrolu yapıldı

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,7 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
@@ -18,12 +19,12 @@ class ArticlesController < ApplicationController
 
     @article = Article.new(article_params)
 
-    @article.save
-
-    redirect_to @article
-
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
-
   private
 
   def article_params
