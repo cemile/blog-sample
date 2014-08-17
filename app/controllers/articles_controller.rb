555eb0c4dd13de4b80b51377daf109aa11212f50
Cemile Sarıcaoğlu <saricaoglucemile@gmail.com>
2014-08-17 19:35:22 +0300

    updated işlemi yapıldı

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,20 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   def new
     @article = Article.new
   end
